7bdc9ac364cdf658f050323957fb0ecee3c3a295
Brian Schlining <bschlining@gmail.com>
2022-12-30 18:00:25 -0800

    initial import

---
+++ scommons/src/main/scala/org/mbari/scommons/App.scala
@@ -0,0 +1,13 @@
+package org.mbari.scommons
+
+/**
+ * @author ${user.name}
+ */
+object App:
+  
+  def foo(x : Array[String]): String = x.foldLeft("")((a,b) => a + b)
+  
+  def main(args : Array[String]): Unit =
+    println( "Hello World!" )
+    println("concat arguments = " + foo(args))
+
