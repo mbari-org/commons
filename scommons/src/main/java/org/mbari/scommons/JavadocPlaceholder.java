452d483431dffee56c59417fc5990e052ac963f4
Brian Schlining <bschlining@gmail.com>
2023-02-09 10:34:45 -0800

    new release

---
+++ scommons/src/main/java/org/mbari/scommons/JavadocPlaceholder.java
@@ -0,0 +1,9 @@
+package org.mbari.scommons;
+
+/**
+ * This class is a placeholder for javadoc. It is used to generate javadoc for
+ * the scommons project.
+ */
+public class JavadocPlaceholder {
+
+}
