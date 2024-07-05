b30443a1683d2ad4d30b501497588c2406110c00
Brian Schlining <bschlining@gmail.com>
2024-07-05 08:16:56 -0700

    Fixed nit

--- scommons/src/main/scala/org/mbari/scommons/util/ISO8601.scala
+++ scommons/src/main/scala/org/mbari/scommons/util/ISO8601.scala
@@ -9,16 +9,14 @@ import org.mbari.scommons.etc.jdk.Instants
 @deprecated("Use org.mbari.scommons.Instants instead", "0.0.7")
 object ISO8601:
 
-
     def parseIso8601(s: String): Either[Throwable, Instant] =
         Instants.parseIso8601(s) match
             case Some(i) => Right(i)
             case None    => Left(new IllegalArgumentException(s"Could not parse $s as an ISO8601 timestamp"))
 
-
-  /**
-   * Parse a string in common ISO8601 formats into an Instant
-   * @param timestamp
-   * @return
-   */
-  def parse(timestamp: String): Option[Instant] = Instants.parseIso8601(timestamp)
+    /**
+     * Parse a string in common ISO8601 formats into an Instant
+     * @param timestamp
+     * @return
+     */
+    def parse(timestamp: String): Option[Instant] = Instants.parseIso8601(timestamp)
